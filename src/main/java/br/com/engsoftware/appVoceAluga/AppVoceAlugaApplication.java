80a6b5404698579a26ddcc4628a05cff0a24fea3
Gabriel Santino <ggs2209@gmail.com>
2023-08-09 00:18:39 -0300

    Project Created

---
+++ src/main/java/br/com/engsoftware/appVoceAluga/AppVoceAlugaApplication.java
@@ -0,0 +1,13 @@
+package br.com.engsoftware.appVoceAluga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVoceAlugaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Projeto Criado Lets Go");
+	}
+
+}
---
+++ src/test/java/br/com/engsoftware/appVoceAluga/AppVoceAlugaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.engsoftware.appVoceAluga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVoceAlugaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
