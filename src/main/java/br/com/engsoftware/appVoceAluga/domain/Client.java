5b5341e6fd6373e876aef8f5a5cc94303a46a00c
Gabriel Santino <ggs2209@gmail.com>
2023-08-14 23:50:46 -0300

    Client Class Created

---
+++ src/main/java/br/com/engsoftware/appVoceAluga/domain/Client.java
@@ -0,0 +1,123 @@
+package br.com.engsoftware.appVoceAluga.domain;
+
+import java.sql.Date;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name="clients") 
+@Table(name="clients") 
+public class Client {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	
+	private String nomeCompleto;
+	
+	private Date dataNascimento;
+	
+	@Column(unique=true)
+	private String cpf;
+	
+	private String endereco;
+	
+	private String numeroTelefone;
+
+	
+	//Getters e Setters 
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getNumeroTelefone() {
+		return numeroTelefone;
+	}
+
+	public void setNumeroTelefone(String numeroTelefone) {
+		this.numeroTelefone = numeroTelefone;
+	}
+
+	// Construtores
+	public Client() {
+		
+	}
+
+	public Client(String nomeCompleto, Date dataNascimento, String cpf, String endereco, String numeroTelefone) {
+		super();
+		this.nomeCompleto = nomeCompleto;
+		this.dataNascimento = dataNascimento;
+		this.cpf = cpf;
+		this.endereco = endereco;
+		this.numeroTelefone = numeroTelefone;
+	}
+	
+	// Hash Code e Equals
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+
+
+}
+
+	
+
+
