4b43c8659b2658bdaab4b1209ed539fc8bdac709
Gabriel Santino <ggs2209@gmail.com>
2023-11-19 01:56:50 -0300

    Atualizado 19/11 01:26 App Rodando sem erros

---
+++ src/main/java/br/com/engsoftware/appVoceAlugaApp/appVoceAlugaApplication.java
@@ -0,0 +1,14 @@
+package br.com.engsoftware.appVoceAlugaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class appVoceAlugaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(appVoceAlugaApplication.class, args);
+    }
+    
+    
+}
